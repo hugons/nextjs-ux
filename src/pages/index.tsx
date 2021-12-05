1e5ab3becabfa6f6452e2d6fe420a9e0c02e3ddf
hugons <hugo@lisbonlab.com>
2021-12-05 20:07:30 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, Welcome to UX united<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>A e-zine about UX and other experiences!</h2>
           <SocialList />
         </div>
       </div>
